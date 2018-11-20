5a8aeba8591c0b4b849e9f8820a3bc44d4676a16
manyagulati123 <mkg8976@g.rit.edu>
2018-11-19 20:58:58 -0500

    Add main script

---
+++ main.py
@@ -0,0 +1,30 @@
+import requests
+import json
+
+def getRepos(url, results):
+  response = requests.get(url + '&per_page=100')
+  repos = json.loads(response.content)
+  headers = response.headers
+  
+  for repo in repos:
+    if repo['private'] == False:
+      results.append(repo['name'])
+
+  if 'Link' in headers:
+    # handle pagination (or the user has more than 100 repositories)
+    links = headers['Link'].split(',')
+    for link in links:
+      if 'next' in link:
+        cleanedLink = link.strip().split(';')[0].replace('<', '').replace('>', '')
+        getRepos(cleanedLink, results)
+        break
+
+
+def main():
+  results = []
+  getRepos("https://api.github.com/users/manyagulati123/repos?", results)
+  
+  return results
+
+
+print(main())
